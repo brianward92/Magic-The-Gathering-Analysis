f70a17aab2ce398d499e00034107b55b6d336a06
Brian Ward <brian.ward.92@gmail.com>
2023-11-05 10:11:32 -0500

    wip: move towards not reading entire file.

--- mtga/base.py
+++ mtga/base.py
@@ -1,24 +1,54 @@
-import pandas as pd
+import csv
+import gzip
+import os
 
 
-CARD_POSITIONS = ["opening_hand", "drawn", "deck", "sideboard"]
+DATA_TYPES = [
+    'draft',
+    'game',
+    'replay',
+]
+
+SETS = [
+    'LTR',
+]
+
+LIMITED_TYPES = [
+    'PremierDraft',
+    'TradDraft',
+    'Sealed',
+    'TradSealed',
+]
+
+CARD_POSITIONS = [
+    "drawn",
+    "deck",
+    "opening_hand",
+    "sideboard",
+    "tutored",
+]
 
 class MTGReader(object):
 
     def __init__(self, set_code, limited_type, data_type, dat_path="~/dat/17Lands"):
-        self.input_data_path = f"{dat_path}/{data_type}_public.{set_code}.{limited_type}.csv"
-        self.data = pd.read_csv(self.input_data_path)
-        # TODO: auto-untar
-        if data_type == "game_data":
-            self._set_game_data_meta()
+        self.file_path = f'{dat_path}/{data_type}_public.{set_code}.{limited_type}.csv.gz'
+        self.file_path = os.path.expanduser(self.file_path)
+        with gzip.open(file_path, 'rt') as file:
+            header = next(csv.reader(file))
+            self.set_card_meta(header)
         return
 
-    def _set_game_data_meta(self):
-        # a check
-        assert len(set(self.data.columns)) == self.data.shape[1], "Duplicated columns!"
+    def file_meta(self, df=None):
+        if (self.file_meta is None) and (df is None):
+            print('WARNING - file_meta not set, ')
+            file_meta
+        return file_meta
+
+    def set_card_meta(self, header):
+        assert len(set(header)) == len(header), "Duplicated columns!"
         self.card_meta = dict()
         self.noncard_columns = []
-        for column in self.data.columns:
+        for column in header:
             is_card = False
             for card_position in CARD_POSITIONS:
                 prefix = f"{card_position}_"
@@ -34,5 +64,5 @@ class MTGReader(object):
                           self.card_meta.items()}
         for i in range(len(CARD_POSITIONS)):
             for j in range(i+1, len(CARD_POSITIONS)):
-                assert self.card_meta[CARD_POSITIONS[i]] == self.card_meta[CARD_POSITIONS[j]]
+                assert card_meta[CARD_POSITIONS[i]] == card_meta[CARD_POSITIONS[j]]
         return
